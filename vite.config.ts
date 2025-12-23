7501f857f840bec5a61c1f07dbb03bafe4419655
xiaobo110 <79360296+xiaobo110@users.noreply.github.com>
2025-12-23 19:04:51 +0800

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/gesture-christmas-tree-2/',
+  base: '/',
   server: {
     port: 3000,
     host: '0.0.0.0',
